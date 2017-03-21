5ae5066c906eed8a2d1766edae51ad72037d346a
teesloane <tylersloane@gmail.com>
2017-03-20 22:58:37 -0400

    clean up chat component

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -4,24 +4,33 @@ import '../../variables.css'
 import Message from '../Message/';
 
 const Chat = props => {
-  const {chatStyle, toggleChat, messages, textBox, handleInput} = props;
 
-  const renderMessages = () => (
-    messages.map(msg => (
-      <Message> {msg.content}</Message>
+  const { chatStyle, toggleChat, messages,
+          textBox, handleInput } = props;
+
+
+  const renderMessages = () => {
+    return messages.map(msg => (
+      <Message type={msg.author}> {msg.content}</Message>
     ))
-  )
-
+  }
 
   return (
     <section class={`Chat-${chatStyle}`}>
 
-      <header class={`Chat__Header-${chatStyle}`} onClick={() => toggleChat(false)}>
+      <header
+        class={`Chat__Header-${chatStyle}`}
+        onClick={ () => toggleChat(false) }
+      >
         Chat with John
       </header>
 
+
       {/* Container for text input and reading messages */}
-      <div class={`Chat__Body-${chatStyle}`}>{renderMessages()}</div>
+      <div class={`Chat__Body-${chatStyle}`}>
+        { renderMessages() }
+      </div>
+
 
       <input
         class={`Chat__Input-${chatStyle}`}
